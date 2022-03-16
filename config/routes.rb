83f432d0fad7aae3876c492d8d829fdbeb612219
Iuliia El Sammak <juliag875@gmail.com>
2022-03-15 23:02:48 -0400

    commit tree a7974fb6c3073261e10f250c5575121609bfcec9

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :
+    # custom route that will do the work of calculating the number of likes and incrementing it
+  patch "/birds/:id/like", to: "birds#increment_likes"
 end
